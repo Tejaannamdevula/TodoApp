c08d446aeb0a59a6751193b0e16b8633dc04128c
Teja Annamdevula <tejaannamdevula@gmail.com>
2024-11-25 18:28:42 +0530

    feat:Initialized nextjs project with shadcn

---
+++ web/client/app/page.js
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col w-screen h-screen items-center justify-center">
+      <Button>TodApp</Button>
+    </div>
+  );
+}
